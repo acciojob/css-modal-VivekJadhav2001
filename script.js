ec54de3f373611ba381c26b0e7b863cd546668d6
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-08-27 14:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const openBtn = document.getElementById('openModal');
+    const modal = document.getElementById('myModal');
+    const closeBtn = document.querySelector('.close-modal');
+
+    openBtn.onclick = () => {
+      modal.style.display = 'flex';
+    };
+
+    closeBtn.onclick = () => {
+      modal.style.display = 'none';
+    };
+
+
+    window.onclick = (e) => {
+      if (e.target === modal) {
+        modal.style.display = 'none';
+      }
+    };
