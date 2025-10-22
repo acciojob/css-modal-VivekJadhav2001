a7b4f4046f4d47b9c732415ff2f7d8e22cf2d4e1
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-22 11:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-const openBtn = document.getElementById("openModal");
-        const modal = document.getElementById("myModal");
-        const closeBtn = document.querySelector(".close-modal");
+//your JS code here. If required.
+const openModalBtn=document.getElementById("openModal");
+const modal=document.querySelector(".modal");
+const closeBtn=document.querySelector(".close-modal");
 
-        // Open
-        openBtn.onclick = () => {
-            modal.style.display = "block";
-        };
+openModalBtn.addEventListener("click",()=>{
+	modal.style.display="block";
+})
+closeBtn.addEventListener("click",()=>{
+	modal.style.display="none";
+})
 
-        // Close (button)
-        closeBtn.onclick = () => {
-            modal.style.display = "none";
-        };
-
-        // Close (outside click)
-        modal.addEventListener("click", (e) => {
-            if (!e.target.closest(".modal-content")) {
-                modal.style.display = "none";
-            }
-        });
+window.addEventListener("click",(e)=>{
+    
+	if (modal.style.display === "block" && modal.contains(e.target)) {
+	    modal.style.display = "none";
+	}          
+})
